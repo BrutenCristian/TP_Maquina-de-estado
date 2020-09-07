e5813592e3883c2f8ae95f194814ef7f80b1e468
BrutenCristian <63882407+BrutenCristian@users.noreply.github.com>
2020-09-07 20:25:51 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,30 +3,28 @@
 
 int main()
 {
-   int dist, estado=0,obs;
+   int dist, estado=0,obs,on,p,e;
    char mov;
    Bienvenida();
-   printf("\nEsta en el rango de distancia?\n1-si\t0-no");
-   fflush(stdin);
-   scanf("%d",&dist);
+   dist=Distancia();
+   do
+   {
    if(d==0)
    {
        do{
        swith(estado)
        {
        case 0://frente
-        obs=Analizo();//devuelve 0 si no hay obstaculo, 1 si lo hay
+        obs=Analizo_Frente();//devuelve 0 si no hay obstaculo, 1 si lo hay
         break;
        case 1://devuelve 0 si no hay obstaculo, 1 si lo hay
-        mov=Giro();//devuelve d si giro a la derecha, sino e
-        obs=Analizo();
+        obs=Analizo_Derecha();
         break;
        case 2://devuelve 0 si hay obstaculo, 1 si lo hay
-        mov=Giro();//devuelve i si giro a la izquierda, sino e
-        obs=Analizo();
+        obs=Analizo_Izquierda();
         break;
        case 3:
-        delay();
+        e=delay();
         break;
        }
        if(obs!=0)
@@ -40,4 +38,12 @@ int main()
        }while(obs!=0);//con el 0 no hay obstaculo
        mov=Avanza();//devuelve a si avanza, sino e
    }
+   else
+   {
+       p=delay();
+   }
+   printf("\nDesea apagar el lazarillo?\n1-no\t0-si");
+   fflush(stdin);
+   scanf("%d",&on);
+   }while(on!=0);
 }
