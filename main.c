d1bd1a063843507fce2826dfbb44e9ce3724d486
BrutenCristian <63882407+BrutenCristian@users.noreply.github.com>
2020-09-07 19:54:52 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Lazarillo.h"
+
 int main()
 {
-    int estado;
-    Bienvenida();
-    do
-    {
-        Funcionamiento();
-        printf("\nDesea que el lazarillo permanezca prendido?");
-        printf("1-Encendido\t2-Apagado");
-        fflush(stdin);
-        scanf("%d",&estado);
-    }while(estado!=2);
+   int dist, estado=0,obs;
+   Bienvenida();
+   printf("\nEsta en el rango de distancia?\n1-si\t0-no");
+   fflush(stdin);
+   scanf("%d",&dist);
+   if(d==0)
+   {
+       do{
+       swith(estado)
+       {
+       case 0://frente
+        obs=Analizo();//devuelve 0 si no hay obstaculo, 1 si lo hay
+        break;
+       case 1://devuelve 0 si no hay obstaculo, 1 si lo hay
+        Giro();
+        obs=Analizo();
+        break;
+       case 2://devuelve 0 si hay obstaculo, 1 si lo hay
+        Giro();
+        obs=Analizo();
+        break;
+       case 3:
+        delay();
+        break;
+       }
+       if(obs!=0)
+       {
+           estado++;
+       }
+       if(estado==4)
+       {
+           estado=0;
+       }
+       }while(obs!=0);//con el 0 no hay obstaculo
+       Avanza();
+   }
 }
